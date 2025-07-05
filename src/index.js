710915dc7d64f0a4b2108fd4e5fc0fbf4cda3847
Rishi Raj <rishiraj438gt@gmail.com>
2025-07-05 17:03:25 +0530

    feat: add cookie-parser and cors middleware, implement async error handling, and create ApiError and ApiResponse classes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-
 dotenv.config({
-    path:'./env'
+  path: "./env",
 });
 
-
-connectDB();
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Failed to connect to the database:", err);
+  });
 
 /*
 import express from "express";
